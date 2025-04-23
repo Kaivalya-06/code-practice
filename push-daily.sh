d93ea7ff0f07850b921e8a96329f8ca00ecddcfc
Kaivalya-06 <kaivaldeshmukh29@gmail.com>
2025-04-23 23:27:13 +0530

    Daily update: 2025-04-23

--- push-daily.sh
+++ push-daily.sh
@@ -1,7 +1,5 @@
-nano push-daily.sh
 #!/bin/bash
-cd /c/Users/KAIVALYA\ DESHMUKH/code-practice || exit
+cd "/c/Users/KAIVALYA DESHMUKH/code-practice" || exit
 git add .
 git commit -m "Daily update: $(date '+%Y-%m-%d')" || echo "No changes to commit"
 git push origin main
-echo "Pushed on $(date)" >> push-log.txt
